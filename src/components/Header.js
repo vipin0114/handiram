26f1ea80b84ab7b6bc1a0754cfa211b7abff66c6
vipin kumar <vipin0114@gmail.com>
2025-12-22 13:01:25 +0000

    responsiveness fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">
         <h1>Handiram</h1>
       </div>
-      <nav>
+      <nav className={isMenuOpen ? 'nav-open' : ''}>
+        <button className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/reservation">Reserve Table</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/about" onClick={() => setIsMenuOpen(false)}>About Us</Link></li>
+          <li><Link to="/menu" onClick={() => setIsMenuOpen(false)}>Menu</Link></li>
+          <li><Link to="/reservation" onClick={() => setIsMenuOpen(false)}>Reserve Table</Link></li>
+          <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link></li>
         </ul>
       </nav>
     </header>
